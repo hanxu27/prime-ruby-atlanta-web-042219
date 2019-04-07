d3a13b9bcd2466880b7d1651ca800df37b82fc3d
han xu <hanxu27@gmail.com>
2019-04-07 00:59:16 +0000

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,30 @@
 # Add  code here!
+def prime?(number)
+  array = []
+  i = 2
+  prime = true
+  counter = 0
+  
+  if number == 2
+    return prime
+  end
+  
+  if number <= 1
+    prime = false
+    return prime
+  end
+  
+  while i <= number/2
+    array << i
+    i += 1
+  end
+  
+  while counter < array.size
+    if number % array[counter] == 0
+      prime = false
+      return prime
+    end
+    counter += 1
+  end
+  prime
+end
